c512b9e5b6dcacad88f1630a3053681b96c5431a
annaq2001 <29985594+annaq2001@users.noreply.github.com>
2019-01-23 13:57:54 -0800

    Lab 4: PivotPoint Class and touched method

--- app/src/main/java/com/example/bruce/snake_startercode/PivotPoint.java
+++ app/src/main/java/com/example/bruce/snake_startercode/PivotPoint.java
@@ -1,4 +1,17 @@
 package com.example.bruce.snake_startercode;
 
 public class PivotPoint {
+
+    private int mX, mY, mDegrees;
+
+    public PivotPoint(int x, int y, int degrees) {
+        mX = x;
+        mY = y;
+        mDegrees = degrees;
+    }
+
+    protected int getX() {return mX;}
+    protected int getY() {return mY;}
+    protected int getDegrees() {return mDegrees;}
+
 }
--- app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SnakeGame {
-  private int mBeginningDirection, mSpriteDim, mBOARD_WIDTH, mBOARD_HEIGHT, mMillisDelay, mScore, mLevel, mCountdown, mXMax, mYMax;
+  private int mSpriteDim, mBOARD_WIDTH, mBOARD_HEIGHT, mMillisDelay, mScore, mLevel, mCountdown, mXMax, mYMax;
   private int[] mAppleCoord;
   private boolean mGameOver = false;
   private List<SnakeSegment> mSnake;
+  private List<PivotPoint> mPivotPoints;
   
   public SnakeGame(int beginningDirection, int beginningSpriteDim, int beginningX, int beginningY, int width, int height){
       mSpriteDim = beginningSpriteDim;
@@ -21,6 +22,7 @@ public class SnakeGame {
       mMillisDelay = 400;
       mAppleCoord = new int[2];
       mSnake = new ArrayList<>();
+      mPivotPoints = new ArrayList<>();
       mSnake.add(new SnakeSegment(SnakeSegment.BodyPart.HEAD, beginningDirection, beginningX, beginningY));
       mSnake.add(new SnakeSegment(SnakeSegment.BodyPart.BODY, beginningDirection, beginningX - 1, beginningY));
       mSnake.add(new SnakeSegment(SnakeSegment.BodyPart.TAIL, beginningDirection, beginningX - 2, beginningY));
@@ -37,7 +39,19 @@ public class SnakeGame {
   protected boolean getGameOver() {return mGameOver;}
   
   protected void touched(float xTouched, float yTouched){
-  
+    int degrees = mSnake.get(0).getDegrees();
+    int xHead = mSnake.get(0).getXLoc();
+    int yHead = mSnake.get(0).getYLoc();
+    int newDegrees = degrees;
+    if (degrees % 180 == 0) {
+        if (yTouched > (yHead * mSpriteDim)) newDegrees = 90;
+        else if (yTouched < ((yHead + 1) * mSpriteDim)) newDegrees = 270;
+    }
+    else if ((degrees + 90) % 180 == 0) {
+        if (xTouched > ((xHead + 1) * mSpriteDim)) newDegrees = 0;
+        else if (xTouched < (xHead * mSpriteDim)) newDegrees = 180;
+    }
+    if (newDegrees != degrees) mPivotPoints.add(new PivotPoint(xHead, yHead, newDegrees));
   }
 
   private void setAppleCoord() {
@@ -51,20 +65,33 @@ public class SnakeGame {
     
   protected boolean play(){
       for (int i = 0; i < mSnake.size(); i++) {
-          int degrees = mSnake.get(i).getDegrees();
           int xLoc = mSnake.get(i).getXLoc();
           int yLoc = mSnake.get(i).getYLoc();
+          if (mPivotPoints.size() != 0) {
+              for (int p = 0; p < mPivotPoints.size(); p++) {
+                  if (xLoc == mPivotPoints.get(p).getX() && yLoc == mPivotPoints.get(p).getY()) {
+                      mSnake.get(i).setDegrees(mPivotPoints.get(p).getDegrees());
+                      if (mSnake.get(i).getBodyPart() == SnakeSegment.BodyPart.TAIL) mPivotPoints.remove(p);
+                      break;
+                  }
+              }
+          }
+          int degrees = mSnake.get(i).getDegrees();
           switch (degrees) {
               case 0:
-              case 180:
                   mSnake.get(i).setXLoc(++xLoc);
                   break;
+              case 180:
+                  mSnake.get(i).setXLoc(--xLoc);
+                  break;
               case 90:
-              case 270:
                   mSnake.get(i).setYLoc(++yLoc);
                   break;
+              case 270:
+                  mSnake.get(i).setYLoc(--yLoc);
+                  break;
           }
-          if (!mGameOver) mGameOver = (xLoc > mXMax || yLoc > mYMax);
+          if (!mGameOver) mGameOver = (xLoc > mXMax || yLoc > mYMax || xLoc < 1 || yLoc < 1);
       }
       return mGameOver;
   }
